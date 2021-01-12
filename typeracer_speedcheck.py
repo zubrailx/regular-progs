8c348d6d65181a3078ad992a77fbfe0d117a7599
Nikita Kulakov <69198006+Zubrailx@users.noreply.github.com>
2021-01-12 20:52:48 +0300

    Update typeracer_speedcheck.py
    сделан вывод по дням

--- typeracer_speedcheck.py
+++ typeracer_speedcheck.py
@@ -1,16 +1,36 @@
 import re
 file = open("file", encoding='utf-8')
 string = file.readline()
+manyStrings = string
 tries = 0
 total = 0
-while string != "":
-    x = re.match(r"^\d*(?= WPM| CPM)", string)
+date = ''
+acc = 0
+while string != '':
+    x = re.search(r"(\d*)( \w{3}\s(.+)%.*?)(\w+\. \d\d, \d\d\d\d)", manyStrings)
     if x:
-        total += int(x.group(0))
+        if date == '':
+            date = x.group(4)
+        elif date != x.group(4):
+            print("\nDate: " + date + '\n')
+            print("Total tries = " + str(tries))
+            print("Average accuracy = " + str(acc/tries))
+            print("Average typing speed = " + str(total/tries)+ "\n------------------------------------------")
+            tries = 0
+            total = 0
+            acc = 0
+            date = ""
+            continue
         tries += 1
-
+        total += int(x.group(1))
+        acc += float(x.group(3))
+        manyStrings = ''
     string = file.readline()
+    manyStrings += string
 
-print(total/tries) 
-
-\\isn't optimised for different days and it doesn't cout normal output (ex, date 21/11/2020 - Typing speed: this)
+# не хочу париться насчет красивого кода(
+print("Date: " + date + '\n')
+print("Total tries = " + str(tries))
+print("Average accuracy = " + str(acc/tries))
+print("Average typing speed = " + str(total/tries))
+print("------------------------------------------")
