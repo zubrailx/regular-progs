38df2ac154f05d1ab90e714a4164b5d041a24854
Nikita Kulakov <69198006+Zubrailx@users.noreply.github.com>
2021-01-12 19:51:06 +0300

    Update typeracer_speedcheck.py
    gave up boolshit

--- typeracer_speedcheck.py
+++ typeracer_speedcheck.py
@@ -1,15 +1,14 @@
 import re
 file = open("file", encoding='utf-8')
 string = file.readline()
-a = []
-t = 0
-suma = 0
+tries = 0
+total = 0
 while string != "":
-    a.append(re.match(r"^\d*", string).group(0))
-    file.readline()
-    file.readline()
+    x = re.match(r"^\d*(?= WPM| CPM)", string)
+    if x:
+        total += int(x.group(0))
+        tries += 1
+
     string = file.readline()
-    t += 1
-for i in a:
-    suma += int(i)
-print(suma/t)
+
+print(total/tries)
