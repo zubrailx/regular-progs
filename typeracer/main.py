9fe5d7fc1efa591bf145958afde7f22935e8be87
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-05-24 00:10:36 +0300

    refactor: change request_type_dict to an array, main() while block

--- typeracer/main.py
+++ typeracer/main.py
@@ -1,33 +1,50 @@
 import typeracer_api as tr
 
 
+def all_statistic():
+    pass
+
+# or today
+def current_day():
+    pass
+
+
+def by_days():
+    pass
+
+
 def main():
-    request_type_dict = {
-        1: tr.all_stat,
-        2: tr.stat_by_current_day,
-        3: tr.stat_by_days
-    }
+    requests = []
+
+    requests.extend([
+        {
+        "description": "Get all statistics",
+        "function": tr.all_stat 
+        },
+        {
+        "description": "Get statistics by current day",
+        "function": tr.stat_by_current_day
+        },
+        {
+        "description": "Get statistics by days",
+        "function": tr.stat_by_days
+        },
+    ])
 
     # main info
     username = input("Enter the username: ")
     universe = input("Enter the universe (or nothing): ")
 
-    # type of request
-    print("Enter the type of request:\n"
-          "1) Get all statistics\n"
-          "2) Get statistics by current day\n"
-          "3) Get statistics by days")
+    # list use requests
+    for i in range(len(requests)):
+        print(f"{i + 1}) {requests[i]['description']}")
 
-    t = None
-    while t is None:
+    while True:
         try:
             t = int(input("Enter the number: "))
-            if t in request_type_dict.keys():
-                request_type_dict[t](username, universe)
-            else:
-                print("request type value is invalid, try again")
-                t = None
-        except ValueError:
+            requests[t - 1]["function"](username, universe)
+            break
+        except (ValueError, IndexError):
             print("request type value is invalid, try again")
 
     input("\nPress Enter to continue...")
@@ -35,4 +52,8 @@ def main():
 
 # this file is a script
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\n")
+        pass
